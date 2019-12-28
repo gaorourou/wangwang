ee7985314edc0b14083ec44d9213d6eabd866a67
gaorourou <2233745288@qq.com>
2019-12-28 11:08:58 +0800

    add setData

--- src/components/ComingSoon/index.vue
+++ src/components/ComingSoon/index.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="movie_body">
+    <meta name="referrer" content="never">
     <ul>
-      <li>
+      <!-- <li>
         <div class="pic_show"><img src="/images/movie_1.jpg"></div>
         <div class="info_list">
           <h2>无名之辈</h2>
@@ -12,86 +13,26 @@
         <div class="btn_pre">
           预售
         </div>
-      </li>
-      <li>
-        <div class="pic_show"><img src="/images/movie_2.jpg"></div>
+      </li> -->
+      <!-- <li v-for="item in comingList" :key="item.id">
+        <div class="pic_show"><img :src="item.img | setWH('128.180')"></div>
         <div class="info_list">
-          <h2>毒液：致命守护者</h2>
-          <p><span class="person">2346</span> 人想看</p>
-          <p>主演: 汤姆·哈迪,米歇尔·威廉姆斯,里兹·阿迈德</p>
-          <p>2018-11-30上映</p>
+          <h2>{{item.nm}}</h2>
+          <p><span class="person">{{item.wish}}</span> 人想看</p><br>
+          <div style="display:inline">主演:</div><p>{{item.star}}</p><br>
+          <p>{{item.showInfo}} 上映</p>
         </div>
         <div class="btn_pre">
           预售
         </div>
-      </li>
-      <li>
-        <div class="pic_show"><img src="/images/movie_1.jpg"></div>
+      </li> -->
+      <li v-for="item in movieList" :key="item.id">
+        <div class="pic_show"><img :src="item.images.small"></div>
         <div class="info_list">
-          <h2>无名之辈</h2>
-          <p><span class="person">17746</span> 人想看</p>
-          <p>主演: 陈建斌,任素汐,潘斌龙</p>
-          <p>2018-11-30上映</p>
-        </div>
-        <div class="btn_pre">
-          预售
-        </div>
-      </li>
-      <li>
-        <div class="pic_show"><img src="/images/movie_2.jpg"></div>
-        <div class="info_list">
-          <h2>毒液：致命守护者</h2>
-          <p><span class="person">2346</span> 人想看</p>
-          <p>主演: 汤姆·哈迪,米歇尔·威廉姆斯,里兹·阿迈德</p>
-          <p>2018-11-30上映</p>
-        </div>
-        <div class="btn_pre">
-          预售
-        </div>
-      </li>
-      <li>
-        <div class="pic_show"><img src="/images/movie_1.jpg"></div>
-        <div class="info_list">
-          <h2>无名之辈</h2>
-          <p><span class="person">17746</span> 人想看</p>
-          <p>主演: 陈建斌,任素汐,潘斌龙</p>
-          <p>2018-11-30上映</p>
-        </div>
-        <div class="btn_pre">
-          预售
-        </div>
-      </li>
-      <li>
-        <div class="pic_show"><img src="/images/movie_2.jpg"></div>
-        <div class="info_list">
-          <h2>毒液：致命守护者</h2>
-          <p><span class="person">2346</span> 人想看</p>
-          <p>主演: 汤姆·哈迪,米歇尔·威廉姆斯,里兹·阿迈德</p>
-          <p>2018-11-30上映</p>
-        </div>
-        <div class="btn_pre">
-          预售
-        </div>
-      </li>
-      <li>
-        <div class="pic_show"><img src="/images/movie_1.jpg"></div>
-        <div class="info_list">
-          <h2>无名之辈</h2>
-          <p><span class="person">17746</span> 人想看</p>
-          <p>主演: 陈建斌,任素汐,潘斌龙</p>
-          <p>2018-11-30上映</p>
-        </div>
-        <div class="btn_pre">
-          预售
-        </div>
-      </li>
-      <li>
-        <div class="pic_show"><img src="/images/movie_2.jpg"></div>
-        <div class="info_list">
-          <h2>毒液：致命守护者</h2>
-          <p><span class="person">2346</span> 人想看</p>
-          <p>主演: 汤姆·哈迪,米歇尔·威廉姆斯,里兹·阿迈德</p>
-          <p>2018-11-30上映</p>
+          <h2>{{item.title}}</h2>
+          <p><span class="person">{{item.collect_count}}</span> 人想看</p><br>
+          <div style="display:inline">主演:</div><p v-for="(cast,index) in item.casts" :key="index">{{cast.name+","}}</p><br>
+          <p>{{item.mainland_pubdate}} 上映</p>
         </div>
         <div class="btn_pre">
           预售
@@ -102,7 +43,39 @@
 </template>
 <script>
 export default {
-  name:"ComingSoon"
+  name:"ComingSoon",
+  data(){
+    return {
+     movieList : [],
+     name : ""
+    }
+  },
+  // mounted(){
+  //   this.axios.get('/api/movieOnInfoList?cityId=10')
+  //   .then((res)=>{
+  //     var msg = res.data.msg;
+  //     if(msg === "ok"){
+  //       var movieList = res.data.data.movieList;
+  //       for(var i=0;i<movieList.length;i++){
+  //         if(movieList[i].sc==0){
+  //           this.comingList.push(movieList[i]);
+  //         }
+  //       }
+  //     }
+  //   })
+  // }
+  mounted(){
+    this.axios.get('https://douban.uieee.com/v2/movie/coming_soon')
+    .then((res)=>{
+      console.log(res);
+      var statusText = res.statusText;
+      console.log(statusText);
+      if(statusText === "OK"){
+        this.movieList = res.data.subjects;
+        console.log(this.movieList);
+      }
+    })
+  }
 }
 </script>
 <style scoped>
@@ -112,7 +85,7 @@ export default {
 .movie_body .pic_show img{ width:100%;}
 .movie_body .info_list { margin-left: 10px; flex:1; position: relative;}
 .movie_body .info_list h2{ font-size: 17px; line-height: 24px; width:150px; overflow: hidden; white-space: nowrap; text-overflow:ellipsis;}
-.movie_body .info_list p{ font-size: 13px; color:#666; line-height: 22px; width:200px; overflow: hidden; white-space: nowrap; text-overflow:ellipsis;}
+.movie_body .info_list p{ font-size: 13px; color:#666; line-height: 22px; width:200px; overflow: hidden; white-space: nowrap; text-overflow:ellipsis;display: inline;}
 .movie_body .info_list .grade{ font-weight: 700; color: #faaf00; font-size: 15px;}
 .movie_body .info_list img{ width:50px; position: absolute; right:10px; top: 5px;}
 .movie_body .btn_mall , .movie_body .btn_pre{ width:47px; height:27px; line-height: 28px; text-align: center; background-color: #f03d37; color: #fff; border-radius: 4px; font-size: 12px; cursor: pointer;}
--- src/components/Search/index.vue
+++ src/components/Search/index.vue
@@ -3,13 +3,13 @@
     <div class="search_input">
       <div class="search_input_wrapper">
         <i class="iconfont icon-sousuo"></i>
-        <input type="text">
+        <input type="text" v-model="message">
       </div>					
     </div>
     <div class="search_result">
       <h3>电影/电视剧/综艺</h3>
       <ul>
-        <li>
+        <!-- <li>
           <div class="img"><img src="/images/movie_1.jpg"></div>
           <div class="info">
             <p><span>无名之辈</span><span>8.5</span></p>
@@ -17,77 +17,14 @@
             <p>剧情,喜剧,犯罪</p>
             <p>2018-11-16</p>
           </div>
-        </li>
-        <li>
-          <div class="img"><img src="/images/movie_1.jpg"></div>
+        </li> -->
+        <li v-for="item in movieList" :key="item.id">
+          <div class="img"><img :src="item.img | setWH('128.180')"></div>
           <div class="info">
-            <p><span>无名之辈</span><span>8.5</span></p>
-            <p>A Cool Fish</p>
-            <p>剧情,喜剧,犯罪</p>
-            <p>2018-11-16</p>
-          </div>
-        </li>
-        <li>
-          <div class="img"><img src="/images/movie_1.jpg"></div>
-          <div class="info">
-            <p><span>无名之辈</span><span>8.5</span></p>
-            <p>A Cool Fish</p>
-            <p>剧情,喜剧,犯罪</p>
-            <p>2018-11-16</p>
-          </div>
-        </li>
-        <li>
-          <div class="img"><img src="/images/movie_1.jpg"></div>
-          <div class="info">
-            <p><span>无名之辈</span><span>8.5</span></p>
-            <p>A Cool Fish</p>
-            <p>剧情,喜剧,犯罪</p>
-            <p>2018-11-16</p>
-          </div>
-        </li>
-        <li>
-          <div class="img"><img src="/images/movie_1.jpg"></div>
-          <div class="info">
-            <p><span>无名之辈</span><span>8.5</span></p>
-            <p>A Cool Fish</p>
-            <p>剧情,喜剧,犯罪</p>
-            <p>2018-11-16</p>
-          </div>
-        </li>
-        <li>
-          <div class="img"><img src="/images/movie_1.jpg"></div>
-          <div class="info">
-            <p><span>无名之辈</span><span>8.5</span></p>
-            <p>A Cool Fish</p>
-            <p>剧情,喜剧,犯罪</p>
-            <p>2018-11-16</p>
-          </div>
-        </li>
-        <li>
-          <div class="img"><img src="/images/movie_1.jpg"></div>
-          <div class="info">
-            <p><span>无名之辈</span><span>8.5</span></p>
-            <p>A Cool Fish</p>
-            <p>剧情,喜剧,犯罪</p>
-            <p>2018-11-16</p>
-          </div>
-        </li>
-        <li>
-          <div class="img"><img src="/images/movie_1.jpg"></div>
-          <div class="info">
-            <p><span>无名之辈</span><span>8.5</span></p>
-            <p>A Cool Fish</p>
-            <p>剧情,喜剧,犯罪</p>
-            <p>2018-11-16</p>
-          </div>
-        </li>
-        <li>
-          <div class="img"><img src="/images/movie_1.jpg"></div>
-          <div class="info">
-            <p><span>无名之辈</span><span>8.5</span></p>
-            <p>A Cool Fish</p>
-            <p>剧情,喜剧,犯罪</p>
-            <p>2018-11-16</p>
+            <p><span>{{item.nm}}</span><span>{{item.sc}}</span></p>
+            <p>{{item.enm}}</p>
+            <p>{{item.cat}}</p>
+            <p>{{item.rt}}</p>
           </div>
         </li>
       </ul>
@@ -96,7 +33,46 @@
 </template>
 <script>
 export default {
-  
+  name:"Search",
+  data(){
+    return {
+      message:'',
+      movieList:[]
+    }
+  },
+  methods : {
+    cancelRequest(){
+      if(typeof this.source ==='function'){
+        this.source('终止请求')
+      }
+    },
+  },
+  watch : {
+    message(newVal){
+      this.cancelRequest();
+      this.axios.get('/api/searchList?cityId=10&kw='+newVal,{
+        cancelToken: new this.axios.CancelToken((c) => {
+          console.log(1);
+          this.source = c;
+        })}).then((res)=>{
+        var msg = res.data.msg;
+        var movies = res.data.data.movies;
+        if(msg && movies){
+          this.movieList = res.data.data.movies.list;
+        }
+        console.log(this.movieList);
+        console.log(res);
+      }).catch((err) => {
+        if (this.axios.isCancel(err)) {
+          console.log('Rquest canceled', err.message); //请求如果被取消，这里是返回取消的message
+        } else {
+            //handle error
+          console.log(err);
+        }
+      })
+    }
+  }
+
 }
 </script>
 <style scoped>
